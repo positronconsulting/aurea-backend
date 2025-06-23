8067c63e92ba8ded6e4cec2118c92a81801d5352
positronconsulting <alfredo@positronconsulting.com>
2025-06-23 15:58:49 -0600

    verificacion.js para arreglar CORS

--- pages/api/verificar-codigo.js
+++ pages/api/verificar-codigo.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end(); // respuesta vacía para preflight
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método no permitido' });
   }
@@ -17,7 +25,6 @@ export default async function handler(req, res) {
     });
 
     const resultado = await respuesta.json();
-
     return res.status(200).json(resultado);
   } catch (error) {
     console.error("Error al llamar a Apps Script:", error);
