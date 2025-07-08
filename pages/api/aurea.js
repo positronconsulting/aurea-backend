60e316fdbb78c3fe600eba03bc86618f1879bdb5
positronconsulting <alfredo@positronconsulting.com>
2025-07-08 12:07:06 -0600

    fucking CORS

--- pages/api/aurea.js
+++ pages/api/aurea.js
@@ -14,7 +14,7 @@ export default async function handler(req) {
       headers: {
         'Access-Control-Allow-Origin': allowedOrigin,
         'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, x-session-id, x-institucion',
+        'Access-Control-Allow-Headers': 'Content-Type, x-session-id, x-institucion, x-tipo',
       },
     });
   }
