22094ffdda30db5961a8ba7846682f3c1115fa88
positronconsulting <alfredo@positronconsulting.com>
2025-07-08 14:24:11 -0600

    PRUEBA TIPO INSTITUCION

--- pages/api/verificar-codigo.js
+++ pages/api/verificar-codigo.js
@@ -1,4 +1,4 @@
-ti default async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://www.positronconsulting.com");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -42,7 +42,7 @@ ti default async function handler(req, res) {
   	acceso: true,
   	institucion: resultado.institucion || "sin nombre",
   	correoSOS: resultado.correoSOS || "",
-  	tipoInstitucion: resultado.tipoInstitucion || "NO ME LLEGO LA INFO";
+  	tipoInstitucion: resultado.tipoInstitucion || "NO ME LLEGO LA INFO"
     });
 
 
